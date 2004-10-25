6a04e2d1e36a0b2bdd9246809713bd14615fabd3
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-10-25 16:33:18 +0000

    adding new version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/OLIN@10261 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fgbg.visu.R
+++ R/fgbg.visu.R
@@ -1,32 +1,36 @@
-fgbg.visu <- function (obj,label=NA)
+fgbg.visu <- function (obj,label)
 {
     Rf <- maRf(obj)
     Gf <- maGf(obj)
-    Rb <- maRb(obj)
-    Gb <- maGb(obj)
+    Rb <- maRb(obj); if (all(dim(Rb)==c(0,0))){Rb <- matrix(1,ncol=dim(Rf)[[2]],nrow=dim(Rf)[[1]])} 
+    Gb <- maGb(obj); if (all(dim(Gb)==c(0,0))){Gb <- matrix(1,ncol=dim(Rf)[[2]],nrow=dim(Rf)[[1]]) }
+    Rb1 <- maRb(obj); if (all(dim(Rb1)==c(0,0))){Rb1 <- matrix(0,ncol=dim(Rf)[[2]],nrow=dim(Rf)[[1]])} 
+    Gb1 <- maGb(obj); if (all(dim(Gb1)==c(0,0))){Gb1 <- matrix(0,ncol=dim(Rf)[[2]],nrow=dim(Rf)[[1]]) }
+    
+
     for (i in 1:dim(maM(obj))[[2]]) {
-        if (is.na(label))label <- paste("Array",i)
+        if (missing(label)) label2 <- paste("Array",i)
         par(mfrow = c(2, 3))
         tmp <- v2m(log2(Rf[, i]), Nsr = maNsr(obj), Nsc = maNsc(obj), 
-            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(0, 
-                max(log2(Rf[, i]))), main = paste(label,
+            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(-max(log2(Rf[, i])), 
+                max(log2(Rf[, i]))), main = paste(label2,
                 ":  log2(Fg)"), visu = TRUE)
         tmp <- v2m(log2(Rb[, i]), Nsr = maNsr(obj), Nsc = maNsc(obj), 
-            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(0, 
-                max(log2(Rb[, i]))), main = "log2(Bg)", visu = TRUE)
-        tmp <- Rf[, i] - Rb[, i]; tmp[tmp <= 0] <- NaN ; 
+            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(-max(log2(Rf[, i])), 
+                max(log2(Rf[, i]))), main = "log2(Bg)", visu = TRUE)
+        tmp <- Rf[, i] - Rb1[, i]; tmp[tmp <= 0] <- NaN ; 
         tmp <- v2m(log2(tmp), Nsr = maNsr(obj), 
             Nsc = maNsc(obj), Ngc = maNgc(obj), Ngr = maNgr(obj), 
             color.lim = c(- log2(max(tmp,na.rm=TRUE)), log2(max(tmp,na.rm=TRUE))),
             main = "log2(Fg-Bg)", 
             visu = TRUE)
         tmp <- v2m(-log2(Gf[, i]), Nsr = maNsr(obj), Nsc = maNsc(obj), 
-            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(-max(log2(Gf[, 
-                i])), 0), main = "log2(Fg)", visu = TRUE)
+            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(-max(log2(Gf[,i])),
+            max(log2(Gf[,i]))), main = "log2(Fg)", visu = TRUE)
         tmp <- v2m(-log2(Gb[, i]), Nsr = maNsr(obj), Nsc = maNsc(obj), 
-            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(-max(log2(Gb[, 
-               i])), 0), main = "log2(Bg)", visu = TRUE)
-       tmp <- Gf[, i] - Gb[, i]; tmp[tmp <= 0] <- NaN ; 
+            Ngc = maNgc(obj), Ngr = maNgr(obj), color.lim = c(-max(log2(Gf[,i])),
+          max(log2(Gf[,i]))), main = "log2(Bg)", visu = TRUE)
+       tmp <- Gf[, i] - Gb1[, i]; tmp[tmp <= 0] <- NaN ; 
        tmp <- v2m(-log2(tmp), Nsr = maNsr(obj), 
             Nsc = maNsc(obj), Ngc = maNgc(obj), Ngr = maNgr(obj), 
             color.lim = c(-max(log2(tmp),na.rm=TRUE),max(log2(tmp),na.rm=TRUE)), 
